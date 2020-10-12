af2e27dda3ce538f30eb78e670fda89a4f0e3d02
thanhhoa214 <hoa.nguyen@nng.bz>
2020-10-12 12:18:51 +0700

    Pre commit for update: angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BoxComponent } from './box/box.component';
 import { TemplateComponent } from './template/template.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { BoxOneComponent } from './box-one/box-one.component';
 import { BoxTwoComponent } from './box-two/box-two.component';
 import { BoxThreeComponent } from './box-three/box-three.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   imports: [BrowserModule],
-  bootstrap: [TemplateComponent],
+  // bootstrap: [TemplateComponent],
   declarations: [
     BoxComponent,
     TemplateComponent,
@@ -19,4 +20,12 @@ import { BoxThreeComponent } from './box-three/box-three.component';
     BoxThreeComponent,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private _injector: Injector) {}
+  ngDoBootstrap() {
+    const element = createCustomElement(TemplateComponent, {
+      injector: this._injector,
+    });
+    customElements.define('custom-template', element);
+  }
+}
